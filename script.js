474b107724a974f0223d5a07afc7977c29b7529f
Your Name <xodjamovaminjon@gmail.com>
2022-09-22 14:43:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let club = prompt(`Как вас зовут?`)
+let age 
+let money
+
+if (club == `A` && `a` || +prompt(`Сколько вам лет?`) >= 20  <= 40) {
+    money = +prompt(`Сколько у тебя денег в кармане?`)
+}
+
+if (money >= 100) {
+    console.log(club);
+} else {
+    alert(`Не тебе ещё!`)
+}
